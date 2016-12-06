c2776313fc896e30aae6d3481a6f94722e09aed5
Panepo <whisper.ko@gmail.com>
2016-12-06 15:13:39 +0800

    update parameter
    update caonnon direct hit damage bonus from +20% to +50%

--- src/constants/ConstParameters.js
+++ src/constants/ConstParameters.js
@@ -4,7 +4,7 @@ export const defaultDef = 100				// define the default defense value
 export const muxPlain = 1.2					// define the multiplier when attacker have plain
 export const muxFlyBow = 1.5				// define the multiplier when bow attack a flying enemy
 export const muxFlyMelee = 0.5			// define the multiplier when melee attack a flying enemy
-export const muxCanDirect = 1.2			// define the multiplier when cannon direct hit
+export const muxCanDirect = 1.5			// define the multiplier when cannon direct hit
 
 export const muxMax = [1.16, 1.32, 1.48, 1.64, 1.8]
 // define the multiplier for each maximum step
