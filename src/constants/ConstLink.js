3729a452b8e553fcd23d1cad93742da5119795cf
Panepo <whisper.ko@gmail.com>
2018-04-25 09:24:39 +0800

    add function
    add generateLink function at Header.jsx to generate hyperlink from
    constLink

---
+++ src/constants/ConstLink.js
@@ -0,0 +1,9 @@
+export const listLink = [
+  { text: '城プロRE', link: 'http://www.dmm.com/netgame_s/oshirore/' },
+  { text: '攻略wiki', link: 'http://scre.swiki.jp/' },
+  {
+    text: 'くろもじ屋',
+    link: 'http://www116.sakura.ne.jp/~kuromoji/castle_enemy.htm'
+  },
+  { text: 'ダメージ計算機', link: 'http://cascal.html.xdomain.jp/cascal/' }
+]
--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -2,10 +2,27 @@ import React, { Component, PropTypes } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { modelOpen, modelClose } from '../actions'
+import { listLink } from '../constants/ConstLink'
 import IndexTable from './IndexTable'
 import '../../css/Header.css'
 
 class Header extends Component {
+  generateLink() {
+    const linkOut = []
+    for (let i = 0; i < listLink.length; i += 1) {
+      let linkTemp = (
+        <a
+          className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary"
+          href={listLink[i].link}
+        >
+          {listLink[i].text}
+        </a>
+      )
+      linkOut.push(linkTemp)
+    }
+    return linkOut
+  }
+
   render() {
     return (
       <header className="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-800">
@@ -15,26 +32,7 @@ class Header extends Component {
             <b>城プロRE 武器傷害機算機 蓬萊パイルバンカー</b>
           </span>
           <div className="mdl-layout-spacer" />
-          <nav className="mdl-navigation">
-            <a
-              className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary"
-              href="http://www.dmm.com/netgame_s/oshirore/"
-            >
-              城プロRE
-            </a>
-            <a
-              className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary"
-              href="http://scre.swiki.jp/"
-            >
-              攻略wiki
-            </a>
-            <a
-              className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary"
-              href="http://www116.sakura.ne.jp/~kuromoji/castle_enemy.htm"
-            >
-              くろもじ屋
-            </a>
-          </nav>
+          <nav className="mdl-navigation">{this.generateLink()}</nav>
         </div>
       </header>
     )
