2701884d66355ebfb4b98a9ec58a9304897e1b83
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-12 18:43:46 +0800

    Deploy Panepo/PileBunker to github.com/Panepo/PileBunker.git:gh-pages

--- precache-manifest.70c2587740eeb228e51e9e9cc4e408ca.js
+++ precache-manifest.70c2587740eeb228e51e9e9cc4e408ca.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/PileBunker/static/js/runtime~main.555c720a.js"
   },
   {
-    "revision": "4679c90ee2ca9a0c2626",
-    "url": "/PileBunker/static/js/main.4679c90e.chunk.js"
+    "revision": "4350ed57925217c6c7b3",
+    "url": "/PileBunker/static/js/main.4350ed57.chunk.js"
   },
   {
     "revision": "94a7f29f35a007ee4af2",
     "url": "/PileBunker/static/js/2.94a7f29f.chunk.js"
   },
   {
-    "revision": "4679c90ee2ca9a0c2626",
+    "revision": "4350ed57925217c6c7b3",
     "url": "/PileBunker/static/css/main.baabff0e.chunk.css"
   },
   {
-    "revision": "174e4df2cc457c1cf3a56e2849786d7a",
+    "revision": "a541ab36d96630f0177fc2354668b945",
     "url": "/PileBunker/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/PileBunker/precache-manifest.6dfc8bf016c1407cbdb36228b9ad483d.js"
+  "/PileBunker/precache-manifest.70c2587740eeb228e51e9e9cc4e408ca.js"
 );
 
 workbox.clientsClaim();
